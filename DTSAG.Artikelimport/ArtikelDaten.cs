b7d7ed52931cb5c7fc3240d0ba04e8667c67e2bf
Administrator <Administrator@SAGE100R80>
2017-07-20 09:28:31 +0200

    Projektdateien hinzufügen.

---
+++ DTSAG.Artikelimport/ArtikelDaten.cs
@@ -0,0 +1,77 @@
+﻿using Sagede.Shared.RealTimeData.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTSAG.Artikelimport
+{
+    public class ArtikelDatenCollection : List<ArtikelDaten>
+    {
+        
+}
+    public class ArtikelDaten
+    {
+        public int ID { get; set; }
+        public string Text { get; set; } //die Daten vom Client Semmikolon separiert
+        public string Artikelnummer { get; set; }
+        public string  Bestellnummer { get; set; }
+        public string Herstellernummer { get; set; }
+        public string Bezeichnung1  { get; set; }
+        public string Bezeichnung2  { get; set; }
+        public string Langtext { get; set; }
+        public decimal Verkaufspreis { get; set; }
+        public decimal Einkaufspreis { get; set; }
+        public string Ersatzartikel { get; set; }
+        public string Gruppe { get; set; }
+        public decimal Gewicht { get; set; }
+        public decimal MwSt { get; set; }
+        public decimal Rabatt { get; set; }
+        public int Mandant { get; set; }
+        public string Erfolgreich { get; set; }
+        public string Gesperrt { get; set; }
+        public string Error { get; set; }
+        public string UUID { get; set; }
+
+        public ArtikelDatenCollection ReadDataSet(string Data)
+        {
+            ArtikelDatenCollection dc = new ArtikelDatenCollection();
+            string[] separator = new string[] { "|@@|" };
+            var result = Data.Split(separator, StringSplitOptions.None).ToList();
+            int index = -1;
+            result.ForEach(r=>{
+                var lineSeparator = new string[] { "@@" };
+                List<string> line = r.Split(lineSeparator, StringSplitOptions.None).ToList();
+                dc.Add(fillArtikeldaten(line,index++));
+            });
+            return dc;
+        }
+
+        private ArtikelDaten fillArtikeldaten(List<string> d, int id)
+        {
+
+            ID = id;
+            //Text 
+            Artikelnummer = d[0];
+            Bestellnummer = d[1];
+            Herstellernummer = d[2];
+            Bezeichnung1 = d[3];
+            Bezeichnung2 = d[4];
+            Langtext = d[5];
+            Verkaufspreis = Conversion.ToDecimal(d[6],true);
+            Einkaufspreis = Conversion.ToDecimal(d[7],true);
+            Ersatzartikel = d[8];
+            Gruppe = d[9];
+            Gewicht = Conversion.ToDecimal(d[10],true);
+            MwSt = Conversion.ToDecimal(d[11],true);
+            Rabatt = Conversion.ToDecimal(d[12],true);
+            Mandant =Conversion.ToInt32(d[13]);
+            Erfolgreich = d[14];
+            Gesperrt = d[15];
+            Error = "";
+            //UUID 
+            return this;
+        }
+    }
+}
---
+++ DTSAG.Artikelimport/Artikelimport.cs
@@ -0,0 +1,140 @@
+﻿using Sagede.OfficeLine.Shared.RealTimeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sagede.Core.Data;
+using Sagede.Shared.RealTimeData.Common;
+using Sagede.OfficeLine.Data;
+using Sagede.OfficeLine.Engine;
+using Sagede.Shared.RealTimeData.Common.Utilities;
+
+namespace DTSAG.Artikelimport
+{
+    class Artikelimport : DataServiceBase
+    {
+        private const string UuidTag = "$uuid";
+        private Mandant _mandant;
+
+        public Artikelimport()
+        {
+            _mandant = Mandant;
+        }
+        public override DataActionResult<DataContainer> Create(DataActionRequest<DataContainer> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataActionResult Delete(DataActionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataServiceExecuteResponse Execute(DataServiceExecuteRequest request)
+        {
+
+            switch (request.MethodName)
+            {
+                //Verfügbare Methoden:
+                //WriteDummy: Erstellt ein Dunmmy Rowset mit der Anzahl der Datenzeilen analog der Zeilenanzahl der Exceldatei, hierdurch ist eine Iteration im Macro möglich
+                // um im nächsten Step Clientseitig die Daten einzulesen
+                case "WriteDummy":
+                    var dto = request.Data;
+                    var uuid = dto.UuidValue;
+                    int count = request.Data.ContainsField("DTCountExcelRows") ? Conversion.ToInt32(request.Data["DTCountExcelRows"]) : 0;
+
+                    RowSet dtoChildrenExceldaten = new RowSet();
+                    
+                    for (int i = 0; i < count; i++)
+                    {
+                        Row dtoChild = new Row();
+                        if (string.IsNullOrEmpty(dtoChild.UuidValue))
+                        {
+                            dtoChild.UuidValue = Guid.NewGuid().ToString();
+                        }
+                        dtoChild.Fill("ID",i);
+                        dtoChildrenExceldaten.Add(dtoChild);
+                    }
+                    //if ((bo.BelegtexteKunden != null))
+                    //{
+                    //    foreach (Sage100.Rezept13.BelegtextKundenItem boChild in bo.BelegtexteKunden)
+                    //    {
+                    //        Row dtoChildBelegtexteKunden = new Row();
+                    //        FillDtoBelegtextKunden(dtoChildBelegtexteKunden, boChild);
+                    //        dtoChildrenBelegtexteKunden.Add(dtoChildBelegtexteKunden);
+                    //    }
+                    //}
+                    //dto.SetChild(dtoChildrenBelegtexteKunden, "BelegtexteKunden");
+                    request.Data.SetChild(dtoChildrenExceldaten,"Importdaten" ); //Name Importdaten ist der virtuelle Name der Detail Datenstruktur 
+                    break;
+                case "WriteData":
+
+                    break;
+
+            }
+            //DataContainer dto = request.Data;
+            //string uuId = dto.UuidValue;// 
+            //string lieferant = dto.ContainsField("DTLieferant") ?Conversion.ToString(dto["DTLieferant"], false):String.Empty;
+            //string vorlage = dto.ContainsField("DTArtikelvorlage") ? Conversion.ToString(dto["DTArtikelvorlage"], false) : String.Empty;
+
+
+            //if (string.IsNullOrEmpty(lieferant))
+            //{
+            //    return DataServiceExecuteResponse.ErrorResponse (new Exception("Es wurde kein Lieferant gewählt!"));
+            //}
+
+            //IGenericCommand command = _mandant.MainDevice.GenericConnection.CreateSqlStringCommand("");
+            return DataServiceExecuteResponse.SuccessResponse(request.Data);
+        }
+
+        public override DataActionResult<DataContainer> GetItem(DataActionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataActionResult<DataContainerSet> GetList(DataActionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataActionResult<DataContainer> GetTemplate(DataActionRequest request)
+        {
+            DataContainer result = new DataContainer();
+            //Dim bo As New DTVorgangsuebersichtVKStatus(Mandant)
+
+            //'UUIDs müssen erzeugt werden
+            result.KeyValue = String.Empty;
+            result.VersionStamp = String.Empty;
+            result.UuidValue = Guid.NewGuid().ToString();
+            result.Fill("DTGuid",result.UuidValue);
+            result.Fill("DTExceldatei", "Excel Datei");
+            result.Fill("DTArtikelvorlage", "Artikel");
+            result.Fill("DTLieferant", "Lieferant");
+            result.Fill("DTCountExcelRows", "2");
+            RowSet dtoChildExceldaten = new RowSet();
+            Row dtoChild = new Row();
+            dtoChild.UuidValue = Guid.NewGuid().ToString();
+            dtoChild.VersionStamp =string.Empty;
+
+            dtoChild.Fill("ID", 1);
+            dtoChild.Fill("Text", "Text");
+            dtoChildExceldaten.Add(dtoChild);
+
+            result.SetChild(dtoChildExceldaten, "Importdaten");
+
+            return DataActionResult.Succeeded(result);
+        }
+
+        public override void PrepareMetadata(DataObjectBase rawMetadata)
+        {
+            DataStructure dataStructure = (DataStructure)rawMetadata;
+
+        }
+
+        public override DataActionResult<DataContainer> Update(DataActionRequest<DataContainer> request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
